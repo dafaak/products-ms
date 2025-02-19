e7e4ec54d959622e59d87891e33319ccb319470d
Israel <israel.pe@outlook.es>
2025-02-19 12:05:35 -0500

    feat: added envs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {ValidationPipe} from "@nestjs/common";
+import {envs} from "./config";
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule);
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            forbidNonWhitelisted: true,
+        })
+    );
+
+    await app.listen(envs.port ?? 3000);
+
+    console.log(`APP running on port ${envs.port}`);
 }
+
 bootstrap();
